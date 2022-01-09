ea1d62104cd24dd0401daea70a9e5ada04661f8b
SaraTague <saramatheney@gmail.com>
2022-01-08 22:24:37 -0600

    updates

--- js/dates.js
+++ js/dates.js
@@ -1,5 +1,2 @@
 const themod = document.querySelector("#lastmod");
 lastmod.textContent = `This page was last updated: ${document.lastModified}`;
-
-const d = new Date();
-const year = d.getFullYear;
