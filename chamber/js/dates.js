d49e3b371226c48cfab0505c26fa3ca9208db4c7
SaraTague <saramatheney@gmail.com>
2022-01-29 21:41:11 -0600

    updates

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -2,3 +2,9 @@ const themod = document.querySelector("#lastmod");
 lastmod.textContent = `Page last updated: ${document.lastModified}`;
 
 year.innerHTML = new Date().getFullYear();
+
+let today = new Date();
+document.getElementById('displayDateTime').innerHTML=today;
+
+document.getElementById("modify").innerHTML = document.lastModified
+document.getElementById("year").innerHTML = new Date().getFullYear()
