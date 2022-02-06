63aa6a4ead017d2436ca71e0f8f82915204821b6
SaraTague <saramatheney@gmail.com>
2022-02-05 21:11:58 -0600

    updates

---
+++ chamber2/js/script.js
@@ -0,0 +1,38 @@
+//last modified date and time
+
+const themod = document.querySelector("#lastmod");
+lastmod.textContent = `Page last updated: ${document.lastModified}`;
+
+//Date for navigation
+
+const datefield = document.querySelector("date");
+const datefieldUK = document.querySelector("aside");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+//Hamberger menu
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+//Popup
+
+let banner = document.querySelector("#banner");
+let dayOfWeek = new Date().getDay();
+
+if (dayOfWeek == 2 || dayOfWeek == 3) {
+  banner.style.display = "block";
+}
