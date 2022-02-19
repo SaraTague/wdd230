8767a418fe0f5e2d5da419ab68a8a0cb618c1e72
SaraTague <saramatheney@gmail.com>
2022-02-18 23:41:46 -0600

    update

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -60,3 +60,21 @@ function windChill(temp, speed) {
   return value;
 }
 
+// Number of visits
+
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+if (numVisits !== 0) {
+  visitsDisplay.textContent = numVisits;
+} else {
+  visitsDisplay.textContent = `This is your first visit!`;
+}
+
+numVisits++;
+
+localStorage.setItem("visits-ls", numVisits);
+
+todayDisplay.textContent = Date.now();
