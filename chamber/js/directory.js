1049461b2d4a535e4c5a20a16912f82d209a6ee4
SaraTague <saramatheney@gmail.com>
2022-03-05 16:49:20 -0600

    updates

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,48 +1,57 @@
-const requestURL =
-  "https://saratague.github.io/wdd230/chamber\data.json"; 
+// const requestURL = 'https://saratague.github.io/wdd230/chamber\data.json';
+const requestURL = "data.json"
+const cards = document.querySelector('div.cards');
+const listButton = document.querySelector("#list-btn");
+const cardButton = document.querySelector("#card-btn");
 
-const cards = document.querySelector(".cards");
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject); // temporary checking for valid response and data parsing
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const businesses = jsonObject['businesses'];
+        // console.table(jsonObject);
+        businesses.forEach(displayBusinesses);
+    });
 
-    const prophets = jsonObject["prophets"];
-    prophets.forEach(displayProphets);
-  });
+function displayBusinesses(business) {
+  let card = document.createElement('section');
 
-function displayProphets(prophet) {
-  // Create elements to add to the document
-  let card = document.createElement("section");
-  let h2 = document.createElement("h2");
-  let birthDate = document.createElement("p");
-  let birthPlace = document.createElement("p");
-  let hrline = document.createElement("hr");
-  let portrait = document.createElement("img");
+  //img
+  let image = document.createElement('img');
+  image.src = business.imgfile;
+  image.setAttribute('alt', business.name);
+  card.appendChild(image);
 
-  // Change the textContent property of the h2 element to contain the prophet's full name
-  h2.textContent = `${prophet.name} ${prophet.lastname}`;
-  birthDate.textContent = `Birth Date: ${prophet.birthdate}`;
-  birthPlace.textContent = `Birth Place: ${prophet.birthplace}`;
+  //h2 name
+  let h3 = document.createElement('h3');
+  h3.textContent = business.name;
+  card.appendChild(h3);
 
-  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-  portrait.setAttribute("src", prophet.imageurl);
-  portrait.setAttribute(
-    "alt",
-    `Portrait of ${prophet.name} ${prophet.lastname} ${prophet.order} Latter-day Saint`
-  );
-  portrait.setAttribute("loading", "lazy");
+  //p address
+  let p1 = document.createElement('p');
+  p1.textContent = business.address;
+  card.appendChild(p1);
 
-  // Add/append the section(card) with the h2 element
-  card.appendChild(h2);
-  card.appendChild(birthDate);
-  card.appendChild(birthPlace);
-  card.appendChild(portrait);
+  //p phone
+  let p2 = document.createElement('p');
+  p2.textContent = business.phone;
+  card.appendChild(p2);
 
-  // Add/append the existing HTML div with the cards class with the section(card)
+  //p website
+  let p3 = document.createElement('p');
+  p3.textContent = business.website;
+  card.appendChild(p3);
+
   cards.appendChild(card);
+
 }
 
+listButton.addEventListener("click", ()=> {
+    cards.classList.replace("card-view", "list-view")
+});
+
+cardButton.addEventListener("click", ()=> {
+    cards.classList.replace("list-view", "card-view")
+});
