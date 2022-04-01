995f0e46d0dc1fb8525b2da503a1026d0f6eddd2
SaraTague <saramatheney@gmail.com>
2022-04-01 11:14:13 -0500

    updates

--- temple-inn-and-suite/js/weather.js
+++ temple-inn-and-suite/js/weather.js
@@ -1,31 +1,80 @@
-const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?zip=75002&units=imperial&appid=ee11ad1912b19a7ddea28dc7f20d38f0";
+  /*apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=75002&units=imperial&appid=ee11ad1912b19a7ddea28dc7f20d38f0';
 
 fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    const t = jsObject.main.temp.toFixed(1);
-    document.querySelector("#current-temp").textContent = t;
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        console.log(jsonObject);
 
-    const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-    const desc = jsObject.weather[0].description;
-    const windsp = jsObject.wind.speed;
-    document.querySelector("#weathericon").setAttribute("src", iconsrc);
-    document.querySelector("#weathericon").setAttribute("alt", desc);
-    document.querySelector("#desc").textContent = desc;
-    document.querySelector("#speed").textContent = windsp;
+        const iconURL = `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
 
-    if (t <= 50 && windsp > 3) {
-      const windchill =
-        35.74 +
-        0.6215 * t -
-        35.75 * Math.pow(windsp, 0.16) +
-        0.4275 * t * Math.pow(windsp, 0.16);
-      document.querySelector("#windchill").innerHTML = `${Math.round(
-        windchill
-      )}&#176;`;
-    } else {
-      document.querySelector("#windchill").innerHTML = "N/A";
+        let temp = jsonObject.main.temp;
+        let speed = jsonObject.wind.speed;
+
+        document.querySelector('.cityName').textContent = jsonObject.name;
+        document.querySelector('figcaption').textContent = jsonObject.weather[0].description;
+        document.querySelector('#weatherIcon').setAttribute('src', iconURL);
+        document.querySelector('#weatherIcon').setAttribute('alt', jsonObject.weather[0].description);
+        document.querySelector('.temp').innerHTML = `Temperature: ${temp} &deg;F`;
+        document.querySelector('.wind-speed').textContent = `Wind Speed: ${speed} mph`;
+        document.querySelector('.humidityDiv').textContent = `Humidity: ${jsonObject.main.humidity}`;
+
+        let windChill = computeWindChill(temp, speed);
+
+        document.querySelector('.wind-chill').textContent = `Wind Chill: ${windChill}`;
+
+        
+    });
+
+    function computeWindChill(t, s) {
+        windchill = Math.round((35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16) * 100 / 100);
+
+        if (t <= 50 && s > 3) {
+            return windchill
+        } else {
+            return `N/A`
+        }
+
+        
     }
-  });
+
+*/
+    
+fetchForecast = function () {
+	var endpoint =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=32.96&lon=-96.79&appid=ee11ad1912b19a7ddea28dc7f20d38f0&units=imperial&lang=en";
+	var forecastEl = document.getElementsByClassName("forecast");
+
+	fetch(endpoint)
+	.then(function (response) {
+		if (200 !== response.status) {
+			console.log(
+				"Looks like there was a problem. Status Code: " + response.status
+			);
+			return;
+		}
+
+		forecastEl[0].classList.add('loaded');
+
+		response.json().then(function (data) {
+			var fday = "";
+			data.daily.forEach((value, index) => {
+				if (index > 0) {
+					var dayname = new Date(value.dt * 1000).toLocaleDateString("en", {
+						weekday: "long",
+					});
+					var icon = value.weather[0].icon;
+					var temp = value.temp.day.toFixed(0);
+					fday = `<div class="forecast-day">
+						<p>${dayname}</p>
+						<p><span class="ico-${icon}" title="${icon}"></span></p>
+						<div class="forecast-day--temp">${temp}<sup>°C</sup></div>
+					</div>`;
+					forecastEl[0].insertAdjacentHTML('beforeend', fday);
+				}
+			});
+		});
+	})
+	.catch(function (err) {
+		console.log("Fetch Error :-S", err);
+	});
+};
